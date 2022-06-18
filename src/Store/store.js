dee2f91ffe62f5c7c8daf0badefeef9bd230df16
klibin <km.putnam2@gmail.com>
2022-06-17 19:31:32 -0600

    added subreddit reducer

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,10 +2,12 @@ import {configureStore} from '@reduxjs/toolkit'
 
 //import reducers
 import setDataReducer from '../Slices/dataSlice'
+import setSubRedditReducer from '../Slices/subRedditSlice'
 
 const store = configureStore({
     reducer: {
       data: setDataReducer,
+      subReddit: setSubRedditReducer
     },
   })
   export default store
