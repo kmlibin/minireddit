bd3853067ed4c983a62b86bd76ba862923c2a608
klibin <km.putnam2@gmail.com>
2022-06-21 14:08:00 -0600

    added search bar and search results page

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,27 +1,27 @@
-// import { useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 //styles
 import './SearchBar.css'
 
 
 export default function Searchbar() {
-    // const [term, setTerm] = useState('')
-    // const navigate = useNavigate()
+    const [term, setTerm] = useState('')
+    const navigate = useNavigate()
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault()
-
-    //     navigate(`/search?query=${term}`)
-    // }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+       
+        navigate(`/search?query=${term}`)
+    }
     return (
         <div className="searchbar">
-            <form>
+            <form onSubmit ={handleSubmit}>
                 <label htmlFor="search">Search:</label>
                 <input
                     type="text"
                     id="search"
-                    // onChange={(e) => setTerm(e.target.value)}
+                    onChange={(e) => setTerm(e.target.value)}
                     required
                 />
             </form>
---
+++ src/Pages/Search/Search.js
@@ -0,0 +1,59 @@
+import { useLocation, Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import { selectPosts } from '../../Slices/dataSlice'
+
+import FeedCard from '../Feed/FeedCard'
+
+export default function Search() {
+    //access to dataslices
+    const posts = useSelector(selectPosts)
+
+    //set up query params
+    const queryString = useLocation().search
+    const queryParams = new URLSearchParams(queryString)
+    const query = queryParams.get('query')
+
+    //check that it's grabbing the term
+    console.log(query)
+
+    //filter through data, push to a new array. use that array to pass values to FeedCard
+    // what about numbers? i.e., I typed in a single digit, and it will find anything with 
+    // the number in ANY of the data(like image links)
+    let results = []
+    const filteredData = posts.forEach((post) => {
+        if (Object.values(post).join('').toLowerCase().includes(query.toLowerCase())) {
+            results.push(post)
+        }
+        return results
+    })
+
+    //check that the results 
+    console.log(results)
+
+    return (
+        <div>
+            {results.length < 1 && (
+                <div>
+                    <h2 className="page-title">Sorry, no results found matching "{query}"
+                    </h2>
+                    <Link to="/">Return to Home</Link>
+                </div>
+            )}
+
+            {results.length > 1 && <h2 className="page-title">Results including "{query}"</h2>}
+            {results.map(post => (
+                <FeedCard
+                    key={post.id}
+                    image={post.image}
+                    title={post.title}
+                    url={post.url}
+                    media={post.media}
+                    text={post.text}
+                    author={post.author}
+                    comments={post.comments}
+                />
+            ))}
+        </div>
+    )
+}
