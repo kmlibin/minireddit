ba77613e604cbb82197832997aaaf9aee4c2754e
klibin <km.putnam2@gmail.com>
2022-06-23 16:07:29 -0600

    formatted the icons in css and in placement

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,21 +1,26 @@
 import { Link } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar'
-import { FaReddit, FaSearch } from 'react-icons/fa'
 
+//react icons
+import { FaReddit } from 'react-icons/fa'
+
+//components
+import SearchBar from '../SearchBar/SearchBar'
+
+//styles
 import './NavBar.css'
 
 export default function NavBar() {
+
   return (
     <div className="navbar">
       <nav>
-        <FaReddit color="var(--reddit-color)" fontSize="2em"/>
+        <FaReddit />
         <Link to="/" className="brand">
           <h1>Reddit<span className="mini">mini</span></h1>
         </Link>
       </nav>
       <div className="search">
         <SearchBar />
-        <FaSearch color="grey" opacity=".5" fontSize="1.2em" />
       </div>
     </div>
   )
