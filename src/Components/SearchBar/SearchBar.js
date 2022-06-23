2d60c94ddc5fccb8126b6dea251d58935377f52f
klibin <km.putnam2@gmail.com>
2022-06-23 16:07:58 -0600

    worked on placeholder value

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,39 +1,52 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-
 import { useSelector } from 'react-redux'
+// import { useSetData } from '../../Hooks/useSetData'
+import { FaReddit, FaSearch } from 'react-icons/fa'
+//selectors
 import { selectPostName } from '../../Slices/dataSlice'
+
 //styles
 import './SearchBar.css'
 
 
 
 export default function Searchbar() {
+    const placeholder = useRef();
     const redditName = useSelector(selectPostName)
     const [term, setTerm] = useState('')
     const navigate = useNavigate()
-   
+    // const {loading} = useSetData()
 
+    //function to clear placeholder value on submit, goes in handlesubmit func
+    const clearInput = () => (placeholder.current.value = "");
+
+    //takes user to the search results page
     const handleSubmit = (e) => {
         e.preventDefault()
-       //clear searchbar on submit
+        clearInput()
         navigate(`/search?query=${term}`)
     }
- 
-    
+
     return (
         <div className="searchbar">
-            <form onSubmit ={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
-                    value={term}
-                    //enter logic for if home component is mounted, it must display something else
-                    placeholder={`Search ${redditName[0]}`}
+                    ref={placeholder}
+                    placeholder="search subreddit"
+                    //this was initially my placeholder, but while the page was loading the bar would say
+                    //"Search undefined". I put it in a condition so it would only show if reddit name was loaded, if 
+                    //the data was loaded, but nothing worked. I tried storing this in state and making the input controlled,
+                    //and nothing changed. I know my conditional "loading &&" was working because I tried it with simple strings
+                    //I would ideally like to show the reddit name in the search bar, just need to figure it out.
+                    // {`Search ${redditName[0]}`}
                     id="search"
                     onChange={(e) => setTerm(e.target.value)}
-                    required
                 />
+
             </form>
+            <FaSearch onClick={handleSubmit} />
         </div>
     )
 }
