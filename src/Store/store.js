c2e169a5c83316c0f98115edd72277d096e98305
klibin <km.putnam2@gmail.com>
2022-06-25 16:02:35 -0600

    deleted comments reducer

--- src/Store/store.js
+++ src/Store/store.js
@@ -3,13 +3,11 @@ import {configureStore} from '@reduxjs/toolkit'
 //import reducers
 import setDataReducer from '../Slices/dataSlice'
 import setSubRedditReducer from '../Slices/subRedditSlice'
-import setCommentsReducer from '../Slices/commentsSlice'
 
 const store = configureStore({
     reducer: {
       data: setDataReducer,
       subReddit: setSubRedditReducer,
-      comments: setCommentsReducer
     },
   })
   export default store
