5e25e4386cc3c80964798f18e05cd1c96b3df3f5
klibin <km.putnam2@gmail.com>
2022-06-25 16:01:46 -0600

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+//pages and components
 import NavBar from './Components/NavBar/NavBar'
 import Home from './Pages/Home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SubReddit from './Pages/SubReddit/SubReddit';
 import SideBar from './Components/SideBar/SideBar';
 import Search from './Pages/Search/Search'
-import Spinner from './Components/Spinner/Spinner';
 
-import { useSetData } from './Hooks/useSetData'
+//styles
+import './App.css';
 
 function App() {
-  const {loading} = useSetData()
+
   return (
     <div className="App">
       <BrowserRouter>
-    {/* <Comments /> */}
         <NavBar />
         <div className="reddit-container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/subreddit/:name" element={<SubReddit />} />
-            <Route path = "/search" element = {<Search />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
           <SideBar />
         </div>
